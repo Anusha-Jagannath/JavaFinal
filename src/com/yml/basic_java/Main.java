45c4745528c3a2b30c57650695a68fb584c6432e
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-25 07:19:11 -0700

    final assignment

---
+++ src/com/yml/basic_java/Main.java
@@ -0,0 +1,58 @@
+package com.yml.basic_java;
+import com.yml.basic_core.*;
+import com.yml.basic_functional.*;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+//		CoinFlip coinflip=new CoinFlip();
+//		coinflip.calculate();
+//		
+//		LeapYear leap=new LeapYear();
+//		leap.findLeap();
+//		
+//		HarmonicNumber harmonic=new HarmonicNumber();
+//		harmonic.series();
+//
+//		
+//		Power2 power=new Power2();
+//		power.findPower(args[0]);
+//		
+//		PrimeFactors prime=new PrimeFactors();
+//		prime.primeFactor();
+//		
+		
+//		QuotientRemainder qr=new QuotientRemainder();
+//		qr.compute();
+		
+//		Swap swap=new Swap();
+//		swap.interchange();
+//		
+//		EvenOdd even=new EvenOdd();
+//		even.findEven();
+//		
+//		Vowel vowel=new Vowel();
+//		vowel.findVowel();
+//		
+//		Largest largest=new Largest();
+//		largest.findLarge();
+//		
+//		ArrayUtil array=new ArrayUtil();
+//		array.getInt2D();
+//		
+//		Triplets triplets=new Triplets();
+//		triplets.findTriplets();
+//		
+//		Distance distance=new Distance();
+//		distance.findDistance(args[0],args[1]);
+//
+//		Quadratic quadratic=new Quadratic();
+//		quadratic.findQuadratic();
+		
+		WindChill wind=new WindChill();
+		wind.findWind(args[0],args[1]);
+	}
+
+}
